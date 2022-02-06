1e8c5fcbc277c94890232ac0aabd6c18bf200b76
suresh <sureshamahendra.gmail.com>
2022-02-06 18:58:23 +0530

    Updates

---
+++ precache-manifest.18254e6aae2fe49f65db9205a294abb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf0e5145194e94ac802a",
+    "url": "/reactapp/static/css/main.5fa11275.chunk.css"
+  },
+  {
+    "revision": "cf0e5145194e94ac802a",
+    "url": "/reactapp/static/js/main.68a313e3.chunk.js"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "0e4007777a51aae15166",
+    "url": "/reactapp/static/js/2.cf2742ac.chunk.js"
+  },
+  {
+    "revision": "6f28e30da33d1f724407c9b9826020ac",
+    "url": "/reactapp/index.html"
+  }
+];
